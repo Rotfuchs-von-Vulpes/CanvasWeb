4dbad3e2b76eaacbaf0339f2240d8b34b3d96f8a
Dr. Vulpes <61910532+Rotfuchs-von-Vulpes@users.noreply.github.com>
2021-05-23 19:18:21 -0400

    desfazendo a merda 02

--- main.js
+++
@@ -1,8 +0,0 @@
-// importando apenas as funções que serão usadas pela aplicação
-import { message, register } from './modules/register.js';
-import { mask } from './modules/dataMask.js';
-
-// tornando as funções do modulo globais
-window.message = message;
-window.register = register;
-window.mask = mask;
